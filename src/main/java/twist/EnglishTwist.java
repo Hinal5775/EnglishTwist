80fe4750234ef57c138d80d53be79607187ae079
Hinal5775 <hinal.shah2@cognizant.com>
2019-02-04 10:59:23 -0600

    refactored: created method to validate input list

--- src/main/java/twist/EnglishTwist.java
+++ src/main/java/twist/EnglishTwist.java
@@ -9,37 +9,36 @@ public class EnglishTwist {
     public List<String> play(List<String> items) {
 
         List<String> result = new ArrayList<>();
-        if (items.isEmpty()) {
+        if (!isInputValid(items)) {
             return result;
-        } else {
+        }
 
-            String previous = items.get(0);
-            if (previous.isEmpty()) {
-                return result;
+        String previous = items.get(0);
+        result.add(previous);
+
+        for (int i = 1; i < items.size(); i++) {
+            String currentWord = items.get(i);
+
+            if (currentWord.isEmpty()) {
+                break;
             }
-            result.add(previous);
+            char lastChar = previous.charAt(previous.length() - 1);
+            char firstChar = currentWord.charAt(0);
 
-            for (int i = 1; i < items.size(); i++) {
-                String currentWord = items.get(i);
-
-                if (currentWord.isEmpty()) {
-                    break;
-                }
-                char lastChar = previous.charAt(previous.length() - 1);
-                char firstChar = currentWord.charAt(0);
-
-                if (lastChar == firstChar) {
-                    result.add(currentWord);
-                } else {
-                    break;
-                }
-
-                previous = currentWord;
+            if (lastChar == firstChar) {
+                result.add(currentWord);
+            } else {
+                break;
             }
 
+            previous = currentWord;
         }
 
         return result;
+    }
+
+    private boolean isInputValid(List<String> items) {
+        return !(items.isEmpty() || items.get(0).isEmpty());
     }
 
 }
